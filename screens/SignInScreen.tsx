b325a1570bacc33cbd77b2a873ca95abfe63f076
JerryG <jgus2012@gmail.com>
2019-08-12 08:40:05 -0700

    use KeyboardAvoidingView for forms

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,42 +1,81 @@
-import React from "react";
-import { View, AsyncStorage } from "react-native";
-import { Button, Divider, Text } from "react-native-paper";
+import React, { useState } from "react";
+import {
+  AsyncStorage,
+  StyleSheet,
+  KeyboardAvoidingView,
+  View
+} from "react-native";
+import {
+  Button,
+  Divider,
+  Text,
+  TextInput,
+  TouchableRipple
+} from "react-native-paper";
 
-const SignInScreen = props => {
-  const handleLogin = async () => {
-    // in real app, call external auth service and set authToken from response
-    // and also set expiration date for auth token
+export default function SignInScreen(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignin = async () => {
+    console.log("Email is: " + email);
+    console.log("Password is: " + password);
+    // TODO: in real app, call external auth service and set authToken from
+    // response and also set expiration date for auth token, show loading indicator
     await AsyncStorage.setItem("@AuthStore:authToken", "abcxsdmadkamda123");
     props.navigation.navigate("Main");
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: 20
-      }}
-    >
-      <Text>This is SignIn screen</Text>
+    <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+      <View>
+        <View style={styles.inputGroup}>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            value={password}
+            secureTextEntry={true}
+            onChangeText={text => setPassword(text)}
+          />
+        </View>
+
+        <Button mode="contained" onPress={handleSignin}>
+          Sign In
+        </Button>
+      </View>
 
       <Divider />
 
-      <Button mode="outlined" onPress={handleLogin}>
-        Login
-      </Button>
-
-      <Divider />
-
-      <Button
-        mode="outlined"
-        onPress={() => props.navigation.navigate("SignUp")}
-      >
-        Go to SignUp screen
-      </Button>
-    </View>
+      <View style={styles.btmTouchText}>
+        <TouchableRipple
+          onPress={() => props.navigation.navigate("SignUp")}
+          rippleColor="rgba(0, 0, 0, .32)"
+        >
+          <Text>I don't have an account.</Text>
+        </TouchableRipple>
+      </View>
+    </KeyboardAvoidingView>
   );
-};
+}
 
-export default SignInScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  inputGroup: {
+    marginBottom: 20
+  },
+  btmTouchText: {
+    marginTop: 20,
+    alignItems: "center"
+  }
+});
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,35 +1,89 @@
-import React from "react";
-import { View } from "react-native";
-import { Button, Divider, Text } from "react-native-paper"
+import React, { useState } from "react";
+import {
+  AsyncStorage,
+  StyleSheet,
+  KeyboardAvoidingView,
+  View
+} from "react-native";
+import {
+  Button,
+  Divider,
+  Text,
+  TextInput,
+  TouchableRipple
+} from "react-native-paper";
 
-const SignUpScreen = props => {
+export default function SignUpScreen(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSignUp = async () => {
+    console.log("Email is: " + email);
+    console.log("Password is: " + password);
+    console.log("Username is: " + username);
+    // TODO: in real app, call external auth service and set authToken from
+    // response and also set expiration date for auth token, show loading indicator
+    await AsyncStorage.setItem("@AuthStore:authToken", "abcxsdmadkamda123");
+    props.navigation.navigate("Main");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: 20
-      }}
-    >
-      <Text>This is SignUp screen</Text>
+    <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+      <View>
+        <View style={styles.inputGroup}>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            mode="outlined"
+            label="Username"
+            value={username}
+            onChangeText={text => setUsername(text)}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            value={password}
+            secureTextEntry={true}
+            onChangeText={text => setPassword(text)}
+          />
+        </View>
+
+        <Button mode="contained" onPress={handleSignUp}>
+          Sign Up
+        </Button>
+      </View>
 
       <Divider />
 
-      <Button
-        mode="outlined"
-        onPress={() => props.navigation.navigate("SignIn")}
-      >
-        Go to SignIn screen
-      </Button>
-
-      <Divider />
-
-      <Button mode="contained" onPress={() => console.log("Sign Up")}>
-        Sign Up
-      </Button>
-    </View>
+      <View style={styles.btmTouchText}>
+        <TouchableRipple
+          onPress={() => props.navigation.navigate("SignIn")}
+          rippleColor="rgba(0, 0, 0, .32)"
+        >
+          <Text>I already have an account</Text>
+        </TouchableRipple>
+      </View>
+    </KeyboardAvoidingView>
   );
-};
+}
 
-export default SignUpScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  inputGroup: {
+    marginBottom: 20
+  },
+  btmTouchText: {
+    marginTop: 20,
+    alignItems: "center"
+  }
+});
