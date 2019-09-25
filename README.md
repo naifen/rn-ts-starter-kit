c7b913c012dbd20129931657d0e0c05325e416a8
JerryG <jgus2012@gmail.com>
2019-09-24 22:27:47 -0700

    add formik and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+## React Native Typescrit Starter Kit
+
+### `yarn start` to run the app
+
+### Features
+
+- Typescript as default.
+- Navigation between different stack of screens.
+- Login, logout with async storage
+
+### Technologies involved
+
+- expo: react-native tool kit.
+- react-navigation: routing & nav
+- react-native-paper: UI library for react-native.
+- formik: form control, validation, etc.
--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   AsyncStorage,
   StyleSheet,
@@ -8,47 +8,99 @@ import {
 import {
   Button,
   Divider,
+  HelperText,
   Text,
   TextInput,
   TouchableRipple
 } from "react-native-paper";
+import { Formik } from "formik";
 
 export default function SignInScreen(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSignin = async () => {
-    console.log("Email is: " + email);
-    console.log("Password is: " + password);
-    // TODO: in real app, call external auth service and set authToken from
-    // response and also set expiration date for auth token, show loading indicator
-    await AsyncStorage.setItem("@AuthStore:authToken", "abcxsdmadkamda123");
-    props.navigation.navigate("Main");
-  };
-
+  // TODO: in real app, call external auth service and set authToken from
+  // response and also set expiration date for auth token, show loading indicator
+  // consider use validationschema https://jaredpalmer.com/formik/docs/guides/validation#validationschema
   return (
     <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
-      <View>
-        <View style={styles.inputGroup}>
-          <TextInput
-            mode="outlined"
-            label="Email"
-            value={email}
-            onChangeText={text => setEmail(text)}
-          />
-          <TextInput
-            mode="outlined"
-            label="Password"
-            value={password}
-            secureTextEntry={true}
-            onChangeText={text => setPassword(text)}
-          />
-        </View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={values => {
+          let errors: { email?: string; password?: string } = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          } else if (!values.password) {
+            errors.password = "Required";
+          } else if (
+            !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/i.test(
+              values.password
+            )
+          ) {
+            errors.password =
+              "Must contain at least one number, one upper case, one lower case & one special character";
+          }
+          return errors;
+        }}
+        onSubmit={async values => {
+          await AsyncStorage.setItem(
+            "@AuthStore:authToken",
+            "abcxsdmadkamda123"
+          );
+          props.navigation.navigate("Main");
+          console.log(values);
+        }}
+      >
+        {formProps => (
+          <View>
+            <View style={styles.inputGroup}>
+              <TextInput
+                mode="outlined"
+                label="Email"
+                value={formProps.values.email}
+                onBlur={formProps.handleBlur("email")}
+                onChangeText={formProps.handleChange("email")}
+              />
+              <HelperText
+                type="error"
+                visible={formProps.errors.email && formProps.touched.email}
+              >
+                {formProps.errors.email}
+              </HelperText>
+              <TextInput
+                mode="outlined"
+                label="Password"
+                secureTextEntry={true}
+                value={formProps.values.password}
+                onBlur={formProps.handleBlur("password")}
+                onChangeText={formProps.handleChange("password")}
+              />
+              <HelperText
+                type="error"
+                visible={
+                  formProps.errors.password && formProps.touched.password
+                }
+              >
+                {formProps.errors.password}
+              </HelperText>
+            </View>
 
-        <Button mode="contained" onPress={handleSignin}>
-          Sign In
-        </Button>
-      </View>
+            <Button
+              mode="contained"
+              disabled={
+                formProps.values.email === "" ||
+                formProps.values.password === "" ||
+                formProps.errors.email ||
+                formProps.errors.password
+              }
+              onPress={formProps.handleSubmit}
+            >
+              Sign In
+            </Button>
+          </View>
+        )}
+      </Formik>
 
       <Divider />
 
